851a0fb13271de7fb60c9b2a00251f856288b4b5
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-11-27 23:39:47 -0700

    adding classes for shape.py

---
+++ tetris-by-marco-varela/game/shape.py
@@ -0,0 +1,19 @@
+from game.actor import Actor
+
+class Shape(Actor):
+    def __init__(self,x_location,y_location):
+        super().__init__()
+        self._image = constants.
+        self._height = constants.
+        self._width = constants.
+
+
+        
+
+    def get_position(self):
+        return self._position
+    
+        
+    
+    
+
