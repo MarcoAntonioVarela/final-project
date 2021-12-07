f9fd389c7b7184e8c97c97854cf6238aa717d2fd
Marco Varela <var17010@byui.edu>
2021-12-06 21:56:08 -0700

    Moving the cast[attacks] into our if satement

--- The C0-vidnator/final_project/game/control_actors_action.py
+++ The C0-vidnator/final_project/game/control_actors_action.py
@@ -3,6 +3,7 @@ from game.sanitizer import Sanitizer
 from game.point import Point
 from game.action import Action
 from game import constants
+from game.attack import Attack
 
 
 class ControlActorsAction(Action):
@@ -10,12 +11,25 @@ class ControlActorsAction(Action):
     def __init__(self, input_service):
 
         self._input_service = input_service
+        self.timer = 0
         
 
     def execute(self,cast):
 
+        self.timer += 1
+
+
         direction = self._input_service.get_direction()
+        space = self._input_service.is_space_pressed()
         sanitizer = cast["sanitizer"][0]
         sanitizer.set_velocity(direction.scale(constants.SANITIZER_SPEED)) 
         
+
+        if space == True and self.timer % 15 == 0:
+            
+            bulled_position = sanitizer.get_position()
+            bullet_x = bulled_position.get_x()
+            bulley_y = bulled_position.get_y()
+            attack = Attack(bullet_x,bulley_y)
+            cast["attacks"].append(attack)
         
