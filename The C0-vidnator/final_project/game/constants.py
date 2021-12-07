6cd16c683102b94b2e5d44886af85a185f32386f
Marco Varela <var17010@byui.edu>
2021-12-07 16:11:51 -0700

    Creating constants and values for covid_attacks

--- The C0-vidnator/final_project/game/constants.py
+++ The C0-vidnator/final_project/game/constants.py
@@ -1,5 +1,7 @@
 import os
 
+
+
 MAX_X = 1000
 MAX_Y = 900
 FRAME_RATE = 30
@@ -11,6 +13,7 @@ DEFAULT_TEXT_OFFSET = 4
 IMAGE_COVID = os.path.join(os.getcwd(), "./final_project/assets/covid.png")
 IMAGE_SANITIZER = os.path.join(os.getcwd(), "./final_project/assets/sanitizer.png")
 IMAGE_ATTACK = os.path.join(os.getcwd(), "./final_project/assets/attack.png")
+IMAGE_COVID_ATTACK = os.path.join(os.getcwd(), "./final_project/assets/covid_attack.png")
 IMAGE_MASK = os.path.join(os.getcwd(), "./final_project/assets/mask.png")
 IMAGE_TITLE = os.path.join(os.getcwd(), "./final_project/assets/title.png")
 IMAGE_CSE210 = os.path.join(os.getcwd(), "./final_project/assets/cse210.png")
@@ -28,8 +31,8 @@ ATTACK_DY = ATTACK_DX * -1
 SANITIZER_X = MAX_X / 2
 SANITIZER_Y = MAX_Y - 25
 
-COVID_WIDTH = 48
-COVID_HEIGHT = 24
+COVID_WIDTH = 67
+COVID_HEIGHT = 70
 
 MASK_WIDTH = 100
 MASK_HEIGHT = 30
@@ -44,9 +47,19 @@ SANITIZER_SPEED = 15
 SANITIZER_WIDTH = 96
 SANITIZER_HEIGHT = 24
 
-ATTACK_WIDTH = 24
-ATTACK_HEIGHT = 24
+ATTACK_WIDTH = 30
+ATTACK_HEIGHT = 30
 
 
 CSE210_WIDTH = 40
 CSE210_HEIGHT = 30
+
+
+COVID_ATTACK_X = MAX_X / 2
+COVID_ATTACK_Y = MAX_Y - 125
+
+COVID_ATTACK_DX = 8
+COVID_ATTACK_DY = ATTACK_DX * -1
+
+COVID_ATTACK_WIDTH = 30
+COVID_ATTACK_HEIGHT = 30
