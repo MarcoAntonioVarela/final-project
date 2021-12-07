8efa2bfa9b962e539424c9735dc29753e4b035b7
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-12-06 22:00:30 -0700

    Update handle_collisions_action.py
    Updating some collisions (specially the attack removing the covid from cast every time it gets git by the bullet)

--- The C0-vidnator/final_project/game/handle_collisions_action.py
+++ The C0-vidnator/final_project/game/handle_collisions_action.py
@@ -9,33 +9,40 @@ class HandleCollisionsAction(Action):
     def execute(self,cast):
         
         physics = PhysicsService()
-        collission_attack= cast["attacks"][0]
+        collission_attack= cast["attacks"]
         collission_sanitizer = cast["sanitizer"][0]
         covids = cast["covids"]
         sound = AudioService()
-    
-        collission = physics.is_collision(collission_attack,collission_sanitizer)
-        
+      
 
-        if collission == True:
-            attack_velocity = collission_attack.get_velocity()
-            sound.play_sound(constants.SOUND_BOUNCE)
-            dy =attack_velocity.get_y()
-            dy *= -1 
-            new_velocity = Point(attack_velocity.get_x(),dy)
-            collission_attack.set_velocity(new_velocity)
- 
         for covid in covids:
-            collided = physics.is_collision(collission_attack,covid)
 
-            if collided == True:
-                sound.play_sound(constants.SOUND_BOUNCE)
-                attack_velocity = collission_attack.get_velocity()
-                dy =attack_velocity.get_y()
-                dy *= -1 
-                new_velocity = Point(attack_velocity.get_x(),dy)
-                collission_attack.set_velocity(new_velocity)
+            for attack in collission_attack:
+
                 
+                collided = physics.is_collision(attack,covid)
 
-                cast["covids"].remove(covid)
+                if collided == True:
+                    sound.play_sound(constants.SOUND_BOUNCE)
+                    attack_velocity = attack.get_velocity()
+                    dy =attack_velocity.get_y()
+                    dy *= -1 
+                    new_velocity = Point(attack_velocity.get_x(),dy)
+                    attack.set_velocity(new_velocity)
+                    
 
+                    cast["covids"].remove(covid)
+                    cast["attacks"].remove(attack)
+
+   # collission = physics.is_collision(collission_attack,collission_sanitizer)
+               
+
+
+        # if collission == True:
+        #     attack_velocity = collission_attack.get_velocity()
+        #     sound.play_sound(constants.SOUND_BOUNCE)
+        #     dy =attack_velocity.get_y()
+        #     dy *= -1 
+        #     new_velocity = Point(attack_velocity.get_x(),dy)
+        #     collission_attack.set_velocity(new_velocity)
+ 
