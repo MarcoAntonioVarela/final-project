e9dd413ab784676f81d15f3d2102247bfba803f9
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-12-06 22:04:34 -0700

    Update move_actors_action.py
    Making magic! this new update will help us to avoid calling each element from cast[] by using a "wildcard" that will help us to iterate through the cast dictionary and then to iterate through all the list within cast

--- The C0-vidnator/final_project/game/move_actors_action.py
+++ The C0-vidnator/final_project/game/move_actors_action.py
@@ -8,29 +8,37 @@ class MoveActorsAction(Action):
 
     def execute(self,cast):
 
-        velocity = cast['attacks'][0].get_velocity()
-        position = cast['attacks'][0].get_position()
 
-        #Merging/separating both x and y values so we can use them in our Point parameters
-        new_x_position = velocity.get_x() + position.get_x() 
-        new_y_position = velocity.get_y() + position.get_y() 
+        for cast_actors in cast:
+
+            for individual_actor in cast[cast_actors]:
+                velocity = individual_actor.get_velocity()
+                position = individual_actor.get_position()
+
+                #Merging/separating both x and y values so we can use them in our Point parameters
+                new_x_position = velocity.get_x() + position.get_x() 
+                new_y_position = velocity.get_y() + position.get_y() 
 
 
-        new_position = Point(new_x_position,new_y_position)
+                new_position = Point(new_x_position,new_y_position)
 
-        cast["attacks"][0].set_position(new_position)
+                individual_actor.set_position(new_position)
+
+
+
+       
         
         ######################################################
         
-        velocity = cast['sanitizer'][0].get_velocity()
-        position = cast['sanitizer'][0].get_position()
+        # velocity = cast['sanitizer'][0].get_velocity()
+        # position = cast['sanitizer'][0].get_position()
 
-        new_x_position = velocity.get_x() + position.get_x() 
+        # new_x_position = velocity.get_x() + position.get_x() 
 
         #The sanitizer (main actor) does not need the y position because we are only moving it left to right
         #new_y_position = velocity.get_y() + position.get_y() 
 
 
-        new_position = Point(new_x_position,position.get_y())
+        # new_position = Point(new_x_position,position.get_y())
 
-        cast["sanitizer"][0].set_position(new_position)
+        # cast["sanitizer"][0].set_position(new_position)
