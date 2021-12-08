335cb14a1fdf1774bb53dbe0387b372b135635ba
Marco Varela <var17010@byui.edu>
2021-12-07 20:07:14 -0700

    Updating the main file

--- The C0-vidnator/final_project/__main__.py
+++ The C0-vidnator/final_project/__main__.py
@@ -7,8 +7,6 @@ from game.title import Title
 from game.cse210 import CSE210
 from game import constants
 from game.director import Director
-from game.actor import Actor
-from game.point import Point
 from game.draw_actors_action import DrawActorsAction
 from game.input_service import InputService
 from game.output_service import OutputService
@@ -18,75 +16,57 @@ from game.covid import Covid
 from game.move_actors_action import MoveActorsAction
 from game.sanitizer import Sanitizer
 from game.mask import Mask
-#from game.covid_attack import Covid_attack
-
-
-#TODO: Add imports similar to the following when you create these classes
-from game.covid import Covid
-from game.attack import Attack
-from game.sanitizer import Sanitizer
 from game.control_actors_action import ControlActorsAction
 from game.handle_collisions_action import HandleCollisionsAction
-from game.move_actors_action import MoveActorsAction
+
 
 def main():
+            
+   
+    #My cast (dictionary) contains several lists, each list represents a group of actors
 
-    # create the cast {key: tag, value: list}
     cast = {} 
+
+    #Each list contains one or many individual actors
+    
     cast["covids"] = []
     cast["attacks"] = []
-   # cast["covid_attacks"]
-    for y in range(4):
-        
+    cast["covid_attacks"] = []
+    cast["masks"]= []
+    cast["titles"]= []
+    cast["CSE210S"]= []
+    cast["sanitizer"] = []
+    sanitizer = Sanitizer(445,790)
+    cast["sanitizer"].append(sanitizer)
+
+
+
+#I have created a for loop in order to create and append several covids (4 lines x 11 columns)
+    for y in range(4):     
         for x in range(11):
             covid = Covid((x*90) ,(y*74))  
             cast["covids"].append(covid)
 
-    
-    cast["masks"]= []
-
+#I have created a for loop in order to create and append 3 covids (1 line x 3 columns)
     for y in range (1,2):
-
         for x in range(0,3):
             mask = Mask((x*370) ,(y*660))
             cast["masks"].append(mask)
 
-
-
-
-    cast["titles"]= []
-
+#I have created a for loop in order to create and append 1 title holding my information 
     for y in range (1,2):
-
         for x in range(1,2):
             title = Title((x*820) ,(y*850))
             cast["titles"].append(title)
 
-
-    cast["CSE210S"]= []
-
+    
+#I have created a for loop in order to create and append 1 title holding the course information
     for y in range (1,2):
-
         for x in range(1,2):
             cse210 = CSE210((x*10) ,(y*850))
             cast["CSE210S"].append(cse210)
 
 
-
-
-    
-    
-
-
-
-
-    # TODO: Create a attack here and add it to the list
-
-    cast["sanitizer"] = []
-
-    sanitizer = Sanitizer(445,790)
-    cast["sanitizer"].append(sanitizer)
-    # TODO: Create a sanitizer here and add it to the list
 
 
     # Create the script {key: tag, value: list}
@@ -120,7 +100,7 @@ def main():
     director = Director(cast, script)
     director.start_game()
 
-    audio_service.stop_audio()
+    audio_service.stop_audio(constants.SOUND_OVER)
 
 if __name__ == "__main__":
     main()
